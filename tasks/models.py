c31458db96e1201de5cf80dd9d4932a7972d98c2
MahmoudEzz <mahmoud.ezz49@gmail.com>
2020-05-31 22:26:49 +0200

    create list table

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# Create Task model consists of two fields text for storing item text and the done for the state of the task
+
+class Task(models.Model):
+    text = models.CharField(max_length=50)
+    done = models.BooleanField(default=False)
+
+    #overwrite default __str__ method to return the object with content name
+    def __str__(self):
+        return self.text
